5588b4a434b50b73a8c5ab96a5f8f0f20e6e117f
Neki <84998222+Nekidev@users.noreply.github.com>
2022-11-27 22:42:35 -0300

    Changed experimental deployment fix

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -6,7 +6,7 @@ export default function Error404() {
             <div className="relative w-full max-w-6xl flex flex-col md:items-center gap-4">
                 <h1 className="text-red-400 text-8xl">404</h1>
                 <div className="text-xl md:text-center">
-                    ¡Uy! No pudimos encontrar eso que estabas buscando. Lo que si encontramos fue un mensaje oculto, en lo mas desierto del desierto que decia <span className={`underline decoration-yellow-400`}>"El partido nashe es el mejor, votenlos"</span>. Al principio no estabamos muy seguros, pero finalmente acordamos que si lo decian los faraones, debe ser verdad.
+                    ¡Uy! No pudimos encontrar eso que estabas buscando. Lo que si encontramos fue un mensaje oculto, en lo mas desierto del desierto que decia <span className={`underline decoration-yellow-400`}>&quot;El partido nashe es el mejor, votenlos&quot;</span>. Al principio no estabamos muy seguros, pero finalmente acordamos que si lo decian los faraones, debe ser verdad.
                 </div>
                 <Link href="/">
                     <button className="mt-4 p-4 text-black font-montserrat bg-white rounded-md transition-transform hover:scale-110 w-full md:w-40">Ir al inicio</button>
