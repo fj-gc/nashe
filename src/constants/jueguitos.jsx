c1318b118df1d24ff44b0a8466049cf838f9ead7
Neki <84998222+Nekidev@users.noreply.github.com>
2022-11-28 16:10:07 -0300

    Removed duplicated constants

--- src/constants/jueguitos.jsx
+++ src/constants/jueguitos.jsx
@@ -1,27 +1,3 @@
-export var jueguitos = [
-    {
-        url: "https://www.meteoblue.com/en/weather/widget/three?geoloc=detect&nocurrent=0&noforecast=0&days=7&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&layout=image",
-        title: "Clima hoy"
-    },
-    {
-        url: "https://oimo.io/works/bubbles/",
-        title: "Burbujitas!"
-    },
-    {
-        url: "https://oimo.io/works/sandlife/",
-        title: "Cuadraditos"
-    },
-    {
-        url: "https://tileslide.com/",
-        title: "Armar foto",
-        scrolling: true
-    },
-    {
-        url: "https://virtualsky.lco.global/embed/index.html?longitude=-58.381592000000005&latitude=-34.603722000000005&projection=stereo&constellations=true&constellationlabels=true&gridlines_eq=true&az=159.99738807263998",
-        title: "El cielo ahora mismo"
-    },
-];
-
 var juegos = [
     {
         url: "https://www.meteoblue.com/en/weather/widget/three?geoloc=detect&nocurrent=0&noforecast=0&days=7&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&layout=image",
